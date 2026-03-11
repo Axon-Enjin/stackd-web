d1ce344f7fb9c9212a3880a999c885767eb8a822
SauceCode01 <sauce.code.01@gmail.com>
2026-03-12 00:47:11 +0800

    feat: utilized different image sizes on testimonials

--- src/features/Testimonials/TestimonialsModule/infrastructure/ImageService.ts
+++ src/features/Testimonials/TestimonialsModule/infrastructure/ImageService.ts
@@ -1,20 +1,26 @@
 import { FilesModuleController } from "@/features/FileSystem/FileSystemModule";
-import { IImageService } from "../domain/IImageService";
+import { IImageService, ImageUploadResult } from "../domain/IImageService";
 
 export class ImageService implements IImageService {
-  constructor(private readonly filesModuleController: FilesModuleController) {}
+  constructor(private readonly filesModuleController: FilesModuleController) { }
 
-  async uploadFile(file: File): Promise<string> {
+  async uploadFile(file: File): Promise<ImageUploadResult> {
     const result = await this.filesModuleController.uploadFile(
       await file.arrayBuffer(),
       file.type,
       file.name,
-      "This file is uploaded by the teams module.",
-      "/teams-module/",
+      "This file is uploaded by the testimonials module.",
+      "/testimonials-module/",
     );
 
-    return result.previewUrl;
+    return {
+      url: result.previewUrl,
+      url64: result.previewUrl64 ?? undefined,
+      url256: result.previewUrl256 ?? undefined,
+      url512: result.previewUrl512 ?? undefined,
+    };
   }
+
   async deleteFile(publicUrl: string): Promise<boolean> {
     return await this.filesModuleController.deleteFileByPreviewUrl(publicUrl);
   }
